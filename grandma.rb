99a136799270c9cd0b797afbb56fba0adfc2b5e2
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-14 18:17:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ if phrase == "I LOVE YOU GRANDMA!"
 return "I lOVE YOU TOO PUMPKIN!"
 elsif phrase == "WHAT DID YOU EAT TODAY?"
 return "NO, NOT SINCE 1938!"
-elsif phrase == " WHAT?"
+elsif phrase == "WHAT?"
 return "NO, NOT SINCE 1938!"
 else
 return "HUH?! SPEAK UP, SONNY!"
